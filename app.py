35d3447a2f41c521cf5773a6a604eac9d30dba55
xjohnsondev <118631229+xjohnsondev@users.noreply.github.com>
2023-07-08 22:02:48 -0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,15 +1,1 @@
-from flask import Flask
-app = Flask(__name__)
-
-
-@app.route('/welcome')
-def welcome_page():
-    return "welcome"
-
-@app.route('/welcome/home')
-def welcome_home():
-    return "welcome home"
-
-@app.route('/welcome/back')
-def welcome_back():
-    return "welcome back"
+# Put your app in here.
