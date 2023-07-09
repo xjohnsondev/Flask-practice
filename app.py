8b9f081857a30caca348781328abb654464c796a
xjohnsondev <118631229+xjohnsondev@users.noreply.github.com>
2023-07-08 22:02:23 -0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
-# Put your app in here.
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/welcome')
+def welcome_page():
+    return "welcome"
+
+@app.route('/welcome/home')
+def welcome_home():
+    return "welcome home"
+
+@app.route('/welcome/back')
+def welcome_back():
+    return "welcome back"
